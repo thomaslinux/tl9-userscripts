636415b7c66dcb12fc5b6779d15a169c7a2dbb82
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-03 16:40:56 +0100

    name

--- wampserver-ahref-projectsli.js
+++ wampserver-ahref-projectsli.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        Liens sur les projets php wampserver
+// @name        wampserver links to projects in project list
 // @namespace   Violentmonkey Scripts
 // @match       http://localhost:8081/*
 // @grant       none
-// @version     2026.03.03.16.36
+// @version     2026.03.03.16.40
 // @author      thomaslinux9
 // @description Liens sur les projets php wampserver
 // ==/UserScript==
