2f310d3e710d9ce46be6b491387303a884c380fc
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-04-13 10:54:16 +0200

    more key actions

--- tl9-key-pencil.user.js
+++ tl9-key-pencil.user.js
@@ -1,24 +1,36 @@
 // ==UserScript==
-// @name        TL9 Keyboard enable disable pencil
+// @name        TL9 Keyboard controls sudoku
 // @namespace   sudoku.com
 // @match       https://sudoku.com/*
 // @icon
 // @grant       none
-// @version     2026.04.11.00.00
+// @version     2026.04.13.10.53
 // @author      thomaslinux
-// @description keyboard controls for the sudoku so space enable you to enable or disable the pencil
+// @description key space, + and z controls for sudoku.com
 // ==/UserScript==
 const HTML_pencil = document.querySelector('div[data-action="pencil"]');
+HTML_pencil.title = '+ for pencil';
+const HTML_pause = document.getElementById('timer-wrapper');
+HTML_pause.title = 'Space to pause';
+const HTML_undo = document.querySelector('div[data-action="undo"]');
+HTML_undo.title = 'z to pause';
 window.addEventListener('keydown', e => {
-if (
-    e.code === 'Space'
-    || e.key === ' '
-    || e.key === '+'
-   ) {
+  if (e.key === '+') {
     e.preventDefault();
     HTML_pencil?.dispatchEvent(
-			new MouseEvent('mousedown')
-		);
+      new MouseEvent('mousedown')
+    );
   }
-}
-);
+  if (e.code === 'Space' || e.key === ' ') {
+    e.preventDefault();
+    HTML_pause?.dispatchEvent(
+      new MouseEvent('click')
+    )
+  }
+  if (e.key === 'z') {
+    e.preventDefault();
+    HTML_undo?.dispatchEvent(
+      new MouseEvent('mousedown')
+    )
+  }
+});
